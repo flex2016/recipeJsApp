c677c4989019f2817e22d5b253c57a974c0eefef
flex2016 <reloded2004@hotmail.com>
2019-04-02 17:44:43 -0400

    added search pagination

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -54,6 +54,37 @@ const renderRecipe = recipe => {
   elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+
+  let button;
+  if (page === 1 && pages > 1) {
+      // Only button to go to next page
+      button = createButton(page, 'next');
+  } else if (page < pages) {
+      // Both buttons
+      button = `
+          ${createButton(page, 'prev')}
+          ${createButton(page, 'next')}
+      `;
+  } else if (page === pages && pages > 1) {
+      // Only button to go to prev page
+      button = createButton(page, 'prev');
+  }
+
+  elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
   // render results of currente page
   const start = (page - 1) * resPerPage;
