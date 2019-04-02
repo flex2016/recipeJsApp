956ba9e8710104788743ce02bd71c3e70e68ca75
flex2016 <reloded2004@hotmail.com>
2019-04-02 15:16:41 -0400

    completed search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,50 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+
+import * as searchView from './views/searchView';
+
+import { elements } from './views/base';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+/**
+ * SEARCH CONTROLLER
+ */
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+      // 2) New search object and add to state
+      state.search = new Search(query);
+
+      // 3) Prepare UI for results
+      searchView.clearInput();
+      searchView.clearResults();
+
+
+      try {
+          // 4) Search for recipes
+          await state.search.getResults();
+
+          // 5) Render results on UI
+
+          searchView.renderResults(state.search.result);
+      } catch (err) {
+          alert('Something wrong with the search...');
+
+      }
+  }
+}
+
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
