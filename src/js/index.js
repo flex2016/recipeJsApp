e11727a491803a39257d2835f92786da9bff3ebf
flex2016 <reloded2004@hotmail.com>
2019-04-03 13:02:03 -0400

    added recipe views

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import Search from './models/Search';
 import Recipe from './models/Recipe';
 
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -68,10 +69,11 @@ const controlRecipe = async () => {
 
   if (id) {
       // Prepare UI for changes
-
+      recipeView.clearRecipe();
+      renderLoader(elements.recipe);
 
       // Highlight selected search item
-
+      if (state.search) searchView.highlightSelected(id);
 
       // Create new recipe object
       state.recipe = new Recipe(id);
@@ -87,7 +89,10 @@ const controlRecipe = async () => {
           state.recipe.calcServings();
 
           // Render recipe
-
+          clearLoader();
+            recipeView.renderRecipe(
+                state.recipe
+            )
 
 
       } catch (err) {
