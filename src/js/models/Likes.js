ab89e39b0fb074a6c425213698601c40f9d39458
flex2016 <reloded2004@hotmail.com>
2019-04-04 14:28:13 -0400

    update

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -7,8 +7,7 @@ export default class Likes {
         const like = { id, title, author, img };
         this.likes.push(like);
 
-        // Perist data in localStorage
-        this.persistData();
+
 
         return like;
     }
@@ -17,8 +16,7 @@ export default class Likes {
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
 
-        // Perist data in localStorage
-        this.persistData();
+
     }
 
     isLiked(id) {
@@ -29,14 +27,5 @@ export default class Likes {
         return this.likes.length;
     }
 
-    persistData() {
-        localStorage.setItem('likes', JSON.stringify(this.likes));
-    }
 
-    readStorage() {
-        const storage = JSON.parse(localStorage.getItem('likes'));
-        
-        // Restoring likes from the localStorage
-        if (storage) this.likes = storage;
-    }
 }
